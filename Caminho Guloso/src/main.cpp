7c938882349796f8a176b7c517e3e1f8012bfaf4
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2023-03-19 09:36:00 -0300

    add config file and some refactors

--- Caminho Guloso/src/main.cpp
+++ Caminho Guloso/src/main.cpp
@@ -6,32 +6,59 @@
 
 using namespace std;
 
-int main() {
-    FILE* file = fopen("matrizes.txt", "r");
+pair<int, int> readInitialPosition() {
+    FILE* file = fopen("../config.va", "r");
+
+    if (file == nullptr) {
+        return {0, 0};
+    }
+
+    pair<int, int> initialPosition;
+
+    fscanf(file, "%d", &initialPosition.first);
+    fscanf(file, "%d", &initialPosition.second);
+
+    return initialPosition;
+}
+
+void readLinesAndColumns(FILE* file, int* lines, int* columns) {
+    fscanf(file, "%d", lines);
+    fscanf(file, "%d", columns);
+}
+
+FILE* openFileToRead(string filename) {
+    FILE* file = fopen(filename.c_str(), "r");
 
     if (file == nullptr) {
         cout << "Error: could not open input file.\n";
         exit(-1);
     }
 
-    int lines;  // Number of lines
-    fscanf(file, "%d", &lines);
+    return file;
+}
 
-    int columns;  // Number of columns
-    fscanf(file, "%d", &columns);
-
-    Matrix matrix(lines, columns);
+int main() {
+    int lines, columns;
+    pair<int, int> initialPosition;
 
     int count = 1;
     int allSum = 0;
+
+    FILE* file = openFileToRead("matrizes.txt");
+
+    pair<int, int> initial = readInitialPosition();
+
+    readLinesAndColumns(file, &lines, &columns);
+
+    Matrix matrix(lines, columns);
 
     while (!feof(file)) {
         cout << "Matriz " << count << ": ";
 
         matrix.readFromFile(file);
 
-        allSum += matrix.solve(0, 0);
-     
+        allSum += matrix.solve(initial.first, initial.second);
+
         count++;
     }
 
